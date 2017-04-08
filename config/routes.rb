eda70ed695c207a5994891f1093bd74aae04d560
jendiamond <thejendiamond@gmail.com>
2017-04-07 23:40:55 -0700

    Change Approach to Design Philosophy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :events
   root "pages#home"
   get '/about', to: 'pages#about'
-  get '/approach', to: 'pages#approach'
+  get '/design_philosophy', to: 'pages#design_philosophy'
   get '/ai_design_tools', to: 'pages#ai_design_tools'
   get '/ai_design_research', to: 'pages#ai_design_research'
   get '/current_ai_projects', to: 'pages#current_ai_projects'
